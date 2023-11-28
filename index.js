a942c86a53458204d48d47f9022df67779592403
Arthur Pereira <arthurjota2@gmail.com>
2023-11-27 22:40:57 -0300

    adição dos icones de lua e sol para trocar de tema

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
